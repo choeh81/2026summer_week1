42f16769f83bcca1d532332f39b863e3b917e907
choeh81 <kwang1122@ewha.ac.kr>
2026-07-02 01:38:57 +0000

    lab4_constructer

--- student.h
+++ student.h
@@ -14,6 +14,13 @@ namespace ChoEunhye2372048
         void calGrade();
 
     public:
+        student(int i = 1234567, int s = 0 /*, char g = 'F'*/) //g없어도 되긴 함
+        : id{i}, score{s}  /*, grade{g}*/  //grade 없어도 되긴 함. 자동으로 계산해주니까
+        {
+            testId();
+            testScore();
+            calGrade(); 
+        }
         void print();
         void input();
         int getId();
